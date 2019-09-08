d237c5a17890aa81d636d8ccf10d33609187993d
KG ZZ <czheng5h88@gmail.com>
2019-09-08 21:17:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.d
+# Write your code here.
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages.append("Hello, my name is ")
+  end
+end
+
+def assign_rooms(speakers)
+  
+end
