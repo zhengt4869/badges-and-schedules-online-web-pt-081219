bc483e369ec362c07dad230b294ed94380999b64
KG ZZ <czheng5h88@gmail.com>
2019-09-08 21:22:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
 def batch_badge_creator(names)
   messages = []
   names.each do |name|
-    messages.append("Hello, my name is #{name}.")
+    messages.append(badge_maker(name))
   end
 end
 
