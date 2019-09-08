7008e00aa2b66c8a8b8a3e19e85da094eb33d9e9
KG ZZ <czheng5h88@gmail.com>
2019-09-08 22:05:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages.append(badge_maker(name))
+  end
+  return messages
+end
+
+def assign_rooms(speakers)
+  res = []
+  speakers.each_with_index do |item, index|
+    res.append("Hello, #{item}! You'll be assigned to room #{index+1}!")
+  end
+  return res
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |str|
+    puts str
+  end
+  assign_rooms(speakers).each do |str|
+    puts str
+  end
+end
